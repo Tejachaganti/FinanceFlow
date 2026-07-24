0cd5eec877e4a8fc62b9f842ed7fa1e2242166cd
Teja <chagantinvsteja@gmail.com>
2026-07-24 15:50:52 +0530

    fix: use BrowserRouter for password reset routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-<BrowserRouter></BrowserRouter>
+
 import App from "./App";
 
 import "./index.css";
@@ -13,12 +13,12 @@ import { FinanceProvider } from "./context/FinanceContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <FinanceProvider>
           <App />
         </FinanceProvider>
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
