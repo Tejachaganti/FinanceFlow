01add7e009a9d11a07da795b5e9edefa0c8dd507
Teja <chagantinvsteja@gmail.com>
2026-07-24 15:49:50 +0530

    fix: add Vercel SPA rewrites

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
+<BrowserRouter></BrowserRouter>
 import App from "./App";
 
 import "./index.css";
